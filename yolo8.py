e97e77903a903c62fc8b1c5eb32de6703b80e93c
gakles <ghorchner@visiometrix.com>
2024-04-26 09:35:25 -0700

    39 images and data.yaml

--- yolo8.py
+++ yolo8.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 # Load the model.
 model = YOLO('yolov8n.pt')
 # Training.
-results = model.train(data='hummingbirds.yaml', imgsz=480, epochs=50, batch=8, name='yolov8n_v8_50e')
+results = model.train(data='data.yaml', imgsz=480, epochs=10, batch=4, name='yolov8n_v8_50e')
