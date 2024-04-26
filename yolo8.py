66183cb97bdefff2f93a85ff0e72ef47df829b72
gakles <ghorchner@visiometrix.com>
2024-04-26 09:55:06 -0700

    now with inference

--- yolo8.py
+++ yolo8.py
@@ -2,4 +2,8 @@ from ultralytics import YOLO
 # Load the model.
 model = YOLO('yolov8n.pt')
 # Training.
+
+	
 results = model.train(data='data.yaml', imgsz=480, epochs=10, batch=4, name='yolov8n_v8_50e')
+
+#yolo task=detect mode=predict model=runs/detect/yolov8n_v8_50e5/weights/best.pt source=inference_data/testbird.png show=True imgsz=480 name=yolov8n_v8_50e_infer480 conf=0.05
