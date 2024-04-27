b464e49e65325d0a04ba59ea4aeb1b4fe1b83e43
gakles <ghorchner@visiometrix.com>
2024-04-27 11:58:09 -0700

    added more images

--- yolo8.py
+++ yolo8.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO 
 # Load the model.
 model = YOLO('yolov8n.pt')
+#model.to('cuda') Need to recompile torch with cuda
 # Training.
 
 	
-results = model.train(data='data.yaml', imgsz=480, epochs=10, batch=4, name='yolov8n_v8_50e')
+results = model.train(data='data.yaml', imgsz=480, epochs=10, batch=4, name='yolov8n_v8_pctest')
 
-#yolo task=detect mode=predict model=runs/detect/yolov8n_v8_50e5/weights/best.pt source=inference_data/testbird.png show=True imgsz=480 name=yolov8n_v8_50e_infer480 conf=0.05
+#yolo task=detect mode=predict model=runs/detect/yolov8n_v8_50e5/weights/best.pt source=inference_data/ show=True imgsz=480 name=yolov8n_v8_50e_infer480 conf=0.05
