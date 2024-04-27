dda921fd878412995dbf47c0566ab7cf47903451
gakles <ghorchner@visiometrix.com>
2024-04-27 12:59:49 -0700

    scorbion

--- yolo8.py
+++ yolo8.py
@@ -5,6 +5,6 @@ model = YOLO('yolov8n.pt')
 # Training.
 
 	
-results = model.train(data='data.yaml', imgsz=480, epochs=10, batch=4, name='yolov8n_v8_pctest')
+results = model.train(data='data.yaml', imgsz=480, epochs=10, batch=4, name='yolov8n_v8_e10')
 
 #yolo task=detect mode=predict model=runs/detect/yolov8n_v8_50e5/weights/best.pt source=inference_data/ show=True imgsz=480 name=yolov8n_v8_50e_infer480 conf=0.05
